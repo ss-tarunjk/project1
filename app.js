dcfd54eeda26f24130c00ae11ca54e9365fbf4f8
Tarun Jaikishan P K <tarun.jaikishan@SSLAP0343.local>
2026-02-03 12:48:24 +0530

    user functions added

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const data = []
+import userFunctions from "./userFunctions.js";
 
+
+userFunctions.addUser({ id: 1, name: "John Doe", email: "john.doe@example.com" })
+userFunctions.addUser({ id: 2, name: "Jane Doe", email: "jane.doe@example.com" })
+userFunctions.addUser({ id: 3, name: "Jim Doe", email: "jim.doe@example.com" })
+userFunctions.addUser({ id: 3, name: "Jim Does", email: "jim.does@example.com" })
+
+console.log(userFunctions.getUsers())
+console.log(userFunctions.getUser(1))
+console.log(userFunctions.deleteUser(10))
+console.log(userFunctions.getUsers())
--- userFunctions.js
+++ userFunctions.js
@@ -1,8 +1,14 @@
-const users = []
+let users = []
 
-const addUser = (user) => {
+const addUser = (newUser) => {
     try {
-        users.push(user)
+        const existingUser = users.find(user => user.id === newUser.id)
+        if (existingUser) {
+            return false
+        }
+
+
+        users.push(newUser)
         return true
     } catch (err) {
         console.error(err)
@@ -10,6 +16,50 @@ const addUser = (user) => {
     }
 }
 
-const Function = { addUser }
+const deleteUser = (id) => {
+    try {
+        const user = users.find(user => user.id === id)
+        if (!user) {
+            return false
+        }
 
-export default Function;
+        users = users.filter(user => user.id !== id)
+        return true
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
+
+const getUser = (id) => {
+    try {
+        const user = users.find(user => user.id === id)
+        return user
+    } catch (err) {
+        console.error(err)
+        return null
+    }
+}
+
+const getUsers = () => {
+    try {
+        return users
+    } catch (err) {
+        console.error(err)
+        return []
+    }
+}
+
+const updateUser = (id, data) => {
+    try {
+        users = users.filter(user => user.id === id ? { ...user, ...data } : user)
+        return true
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
+
+const userFunctions = { addUser, deleteUser, getUser, getUsers, updateUser }
+
+export default userFunctions;
